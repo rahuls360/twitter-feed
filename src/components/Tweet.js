a0095f798ea0301a5a3971cd328a5059bcbbdbdb
Rahul Makhija <rahuls360@gmail.com>
2019-02-13 17:49:35 +0530

    add bootstrap well to tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
-class Tweet extends React.Component{
-    render(){
-        return <p>{this.props.tweets[this.props.index]}</p>
-    }
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div className="well">
+        <p>{this.props.tweets[this.props.index]}</p>
+      </div>
+    );
+  }
 }
 
 export default Tweet;
