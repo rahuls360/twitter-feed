9695d5612b5e614fa47e5eb8e9cec978f87699ab
Rahul Makhija <rahuls360@gmail.com>
2019-02-22 21:19:36 +0530

    styling button to next line

--- src/components/PostTweet.js
+++ src/components/PostTweet.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
 
-class PostTweet extends React.Component{
-    postTweetRef = React.createRef();
-    handleTextbox = (event) => {
-        this.props.addTweet(this.postTweetRef.current.value);
-        event.currentTarget.reset()
-        event.preventDefault();
-    }
-    render(){
-        return <div className="tweetbox">
+class PostTweet extends React.Component {
+  postTweetRef = React.createRef();
+  handleTextbox = event => {
+    this.props.addTweet(this.postTweetRef.current.value);
+    event.currentTarget.reset();
+    event.preventDefault();
+  };
+  render() {
+    return (
+      <div className="tweetbox">
         <form onSubmit={this.handleTextbox}>
-          <textarea name="tweet" id="" cols="30" rows="5" ref={this.postTweetRef} placeholder="Enter Tweet"></textarea>
-          <input type="submit" value="Post Tweet"/>
+          <textarea
+            name="tweet"
+            id=""
+            cols="30"
+            rows="5"
+            ref={this.postTweetRef}
+            placeholder="Enter Tweet"
+          />
+          <br />
+          <input type="submit" value="Post Tweet" />
         </form>
-        </div>
-    }
+      </div>
+    );
+  }
 }
 export default PostTweet;
