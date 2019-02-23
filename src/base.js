4be66acde0154c4eb812f1dd18c2db3245013ae8
Rahul Makhija <rahuls360@gmail.com>
2019-02-23 13:13:12 +0530

    fix security issue

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import firebase from "firebase";
 import Rebase from "re-base";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCmcD2j1FbQsq9TgoH20TFD6CFmU7BQLOk",
-  authDomain: "twitter-feed-48501.firebaseapp.com",
-  databaseURL: "https://twitter-feed-48501.firebaseio.com",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
   projectId: "twitter-feed-48501",
   storageBucket: "twitter-feed-48501.appspot.com",
   messagingSenderId: "645310389452"
