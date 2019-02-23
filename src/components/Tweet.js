4039b7cba357861b0c2131e8e3532481ada49554
Rahul Makhija <rahuls360@gmail.com>
2019-02-23 11:59:15 +0530

    add edit and delete buttons

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Tweet extends React.Component {
   render() {
     return (
       <div className="well">
-        <p>{this.props.tweets[this.props.index]}</p>
+        <p>
+          {this.props.tweets[this.props.index]}
+          <span className="left-margin">
+            <Link to="/edit">Edit</Link>
+            <Link to="/delete">Delete</Link>
+          </span>
+        </p>
       </div>
     );
   }
